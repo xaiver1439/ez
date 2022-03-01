78f947a5c836b2e530ef855bf1546747ac4a1795
xaiver1439 <100653655+xaiver1439@users.noreply.github.com>
2022-03-01 09:48:23 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,65 @@
+let roll = 0
+input.onGesture(Gesture.Shake, function () {
+    roll = randint(1, 6)
+    if (roll == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(1000)
+    } else if (roll == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(1000)
+    } else if (roll == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(1000)
+    } else if (roll == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(1000)
+    } else if (roll == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(1000)
+    } else if (roll == 6) {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            `)
+        basic.clearScreen()
+        basic.pause(1000)
+    }
+})
